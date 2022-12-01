e37ad115b97f8b46dcaa43506c314df5354aa016
Lisa <S1174662@student.windesheim.nl>
2022-12-01 13:38:06 +0100

    Last version sprint 5

--- AccountSettings/accountSettings.php
+++ AccountSettings/accountSettings.php
@@ -6,70 +6,36 @@ $user = unserialize($_SESSION['gebruiker_data']);
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="UTF-8" />
-    <link rel="stylesheet" href="accountSettings.css" />
-    <link rel="stylesheet" href="../style.css" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
-    <title>Account aanpassen</title>
-  </head>
-  <body>
-    <!--Navbar Import -->
-    <div id="nav-placeholder"></div>
 
-    <script>
-      $(function () {
-        $("#nav-placeholder").load("../navBar.php");
-      });
-    </script>
-    <div class="registreren">
-      <h1>Account aanpassen</h1>
-      <form method="post" action="../forms/accountSettings-forms.php">
-        
-          <input
-            class="inputnaam"
-            type="text"
-            id="voornaam"
-            name="voornaam"
-            placeholder="Voornaam"
-          />
-          <input
-            class="inputnaam"
-            type="text"
-            id="tussenvoegsel"
-            name="tussenvoegsel"
-            placeholder="tussenvoegsel"
-          />
-          <input
-            class="inputnaam"
-            type="text"
-            id="achternaam"
-            name="achternaam"
-            placeholder="Achternaam"
-          />
-        <input
-          class="inputgegevens"
-          type="text"
-          id="email"
-          name="email"
-          placeholder="Email"
-        />
-        <input
-          class="inputgegevens"
-          type="password"
-          id="wachtwoord"
-          name="wachtwoordcheck"
-          placeholder="Wachtwoord"
-        />
-        <input
-          class="inputgegevens"
-          type="password"
-          id="wachtwoord"
-          name="wachtwoord"
-          placeholder="Nieuw Wachtwoord"
-        /><br>
-        <!-- <div class="usertype">
+<head>
+  <meta charset="UTF-8" />
+  <link rel="stylesheet" href="accountSettings.css" />
+  <link rel="stylesheet" href="../style.css" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+  <title>Account aanpassen</title>
+</head>
+
+<body>
+  <!--Navbar Import -->
+  <div id="nav-placeholder"></div>
+
+  <script>
+    $(function() {
+      $("#nav-placeholder").load("../navBar.php");
+    });
+  </script>
+  <div class="registreren">
+    <h1>Account aanpassen</h1>
+    <form method="post" action="../forms/accountSettings-forms.php">
+
+      <input class="inputnaam" type="text" id="voornaam" name="voornaam" placeholder="Voornaam" />
+      <input class="inputnaam" type="text" id="tussenvoegsel" name="tussenvoegsel" placeholder="tussenvoegsel" />
+      <input class="inputnaam" type="text" id="achternaam" name="achternaam" placeholder="Achternaam" />
+      <input class="inputgegevens" type="text" id="email" name="email" placeholder="Email" />
+      <input class="inputgegevens" type="password" id="wachtwoord" name="wachtwoordcheck" placeholder="Wachtwoord" />
+      <input class="inputgegevens" type="password" id="wachtwoord" name="wachtwoord" placeholder="Nieuw Wachtwoord" /><br>
+      <!-- <div class="usertype">
           <select
             required
             class="usertype-dropdown"
@@ -84,27 +50,24 @@ $user = unserialize($_SESSION['gebruiker_data']);
             <option value="non-actief">Non-actief</option>
           </select>
         </div> -->
-          <label>Profielfoto</label><br />
-          <input
-            type="file"
-            id="myfile"
-            name="myfile"
-            accept="image/png, image/jpeg"
-          />
-        <div class="btn-group">
-          <button name="submit" class="submit">Aanpassen</button>
-        </div>
-        <div class="error">
+      <label>Profielfoto</label><br />
+      <input type="file" id="myfile" name="myfile" accept="image/png, image/jpeg" />
+      <div class="btn-group">
+        <button name="submit" class="submit">Aanpassen</button>
+      </div>
+      <div class="error">
         <?php
         // laat error code Zien
         if (isset($_SESSION['ERRORS'])) {
           echo $_SESSION['ERRORS'];
           unset($_SESSION['ERRORS']);
         }
-       if (isset($_SESSION['succes'])) {
+        if (isset($_SESSION['succes'])) {
           echo $_SESSION['succes'];
-          unset($_SESSION['succes']); }?></div>
-      </form>
-    </div>
-  </body>
+          unset($_SESSION['succes']);
+        } ?></div>
+    </form>
+  </div>
+</body>
+
 </html>
