30dcbbd891d3798adf0d4ec08aec666e44ef4bd4
Eric Mensah <s1175134@student.windesheim.nl>
2022-12-02 10:46:03 +0100

    time change

--- UrenRegistratie/urenRegistratie.php
+++ UrenRegistratie/urenRegistratie.php
@@ -62,19 +62,45 @@ require_once("../src/sessie.php");
 
         <div class="tijd">
           <label>Begonnen om
-            <input type="time" class="tijd-input" name="tijd"/></label>
+            <input id="start" type="time" class="tijd-input" name="tijd"/></label>
           <div class="eindtijd">
             <label>Beïndigd om
-              <input type="time" class="tijd-input" name="tijd"/></label>
+              <input id="end" type="time" class="tijd-input" name="tijd"/></label>
           </div>
           <div class="toteindtijd">
             <label>Totale gewerkte tijd
-              <input class="tijd-input" name="tijd"/></label>
+              <input id="diff" class="tijd-input" name="tijd"/></label>
           </div>
           <button class="submit">Toevoegen</button>
         </div>
       </form>
   </div>
 </body>
+<script>
+var start = document.getElementById("start").value;
+var end = document.getElementById("end").value;
+
+document.getElementById("start").onchange = function() {diff(start,end)};
+document.getElementById("end").onchange = function() {diff(start,end)};
+
+
+function diff(start, end) {
+    start = document.getElementById("start").value; //to update time value in each input bar
+    end = document.getElementById("end").value; //to update time value in each input bar
+    
+    start = start.split(":");
+    end = end.split(":");
+    var startDate = new Date(0, 0, 0, start[0], start[1], 0);
+    var endDate = new Date(0, 0, 0, end[0], end[1], 0);
+    var diff = endDate.getTime() - startDate.getTime();
+    var hours = Math.floor(diff / 1000 / 60 / 60);
+    diff -= hours * 1000 * 60 * 60;
+    var minutes = Math.floor(diff / 1000 / 60);
+
+    return (hours < 9 ? "0" : "") + hours + ":" + (minutes < 9 ? "0" : "") + minutes;
+}
+
+setInterval(function(){document.getElementById("diff").value = diff(start, end);}, 1000); //to update time every second (1000 is 1 sec interval and function encasing original code you had down here is because setInterval only reads functions) You can change how fast the time updates by lowering the time interval
+</script>
 
 </html>
